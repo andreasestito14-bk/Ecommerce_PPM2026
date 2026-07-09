aa4b580606411a8dd4e65d4caae29d413ea2054f
andre <andreasestito14@gmail.com>
2026-07-09 14:59:56 +0200

    Configurazione per il deploy su Render

--- ecommerce_project/settings.py
+++ ecommerce_project/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-(9f0wl4&lp6--8aty5p4=5g^m=fdhu$(*m$#!r6y_%)-dzij2f
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -117,5 +118,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 AUTH_USER_MODEL = 'accounts.CustomUser'
